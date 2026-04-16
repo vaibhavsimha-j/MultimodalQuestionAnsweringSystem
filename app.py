e7a19039266728e56cbe7859baf91f02765caf17
Vaibhav Simha J <vaibhavsimhajworks@gmail.com>
2026-04-16 18:57:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.set_page_config(
 st.markdown("""
 <style>
   /* ── global background ── */
-  .stApp { background-color: #BCB0F7; color: #e0e0f0; }
+  .stApp { background-color: #38305C; color: #e0e0f0; }
 
   /* ── sidebar ── */
   section[data-testid="stSidebar"] {
